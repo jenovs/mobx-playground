cdc8ee9daacb5d8f6d4831518864cf2b8e4df28c
Viktors Jenovs <jviktors@gmail.com>
2018-07-29 18:52:09 +0300

    Add NumInput to Card

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 
+import NumInput from './NumInput';
+
 const style = {
+  numInput: {
+    display: 'flex',
+  },
   wrapper: {
     display: 'flex',
     justifyContent: 'space-around',
@@ -11,15 +16,24 @@ const style = {
 };
 
 const Card = inject('data')(
-  observer(({ data, id, from, to, price }) => (
-    <div style={style.wrapper} data-testid="card">
-      <div>1 {from}</div>
-      <div>
-        {price || '...'} {to}
+  observer(({ amount, data, id, from, to, price }) => {
+    const total = price * (amount === undefined ? 1 : amount);
+    const formatTotal = (t: number) => {
+      return t >= 1 ? t.toFixed(2) : t > 0 ? t : '...';
+    };
+
+    return (
+      <div style={style.wrapper} data-testid="card">
+        <div style={style.numInput}>
+          <NumInput id={id as number} /> {from}
+        </div>
+        <div>
+          {formatTotal(total)} {to}
+        </div>
+        <button onClick={() => data.deletePair(id)}>Delete</button>
       </div>
-      <button onClick={() => data.deletePair(id)}>Delete</button>
-    </div>
-  ))
+    );
+  })
 );
 
 export default Card;
