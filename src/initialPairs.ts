ed3b938d3fe77225eeeaa15135a87e02aee783c5
Viktors Jenovs <jviktors@gmail.com>
2018-08-05 14:29:13 +0300

    Create `initialPairs`

---
+++ src/initialPairs.ts
@@ -0,0 +1,9 @@
+import { IPairs } from './Store';
+
+export const initialPairs: IPairs[] = [
+  { id: 0, from: 'BTC', to: 'EUR', amount: '4.567' },
+  { id: 1, from: 'LTC', to: 'EUR', amount: '189.456987' },
+  { id: 42, from: 'ETH', to: 'USD', amount: '1' },
+  { id: 69, from: 'EUR', to: 'USD', amount: '1' },
+  { id: 69, from: 'EUR', to: 'CHF', amount: '1' },
+];
