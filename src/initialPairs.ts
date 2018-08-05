49c21da765000fc36fac0621721675c220a83d5e
Viktors Jenovs <jviktors@gmail.com>
2018-08-05 14:35:30 +0300

    Update initialPairs.ts

--- src/initialPairs.ts
+++ src/initialPairs.ts
@@ -5,5 +5,5 @@ export const initialPairs: IPairs[] = [
   { id: 1, from: 'LTC', to: 'EUR', amount: '189.456987' },
   { id: 42, from: 'ETH', to: 'USD', amount: '1' },
   { id: 69, from: 'EUR', to: 'USD', amount: '1' },
-  { id: 69, from: 'EUR', to: 'CHF', amount: '1' },
+  { id: 70, from: 'EUR', to: 'CHF', amount: '1' },
 ];
